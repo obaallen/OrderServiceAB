c414782736044c07683a74e6aac4b0ec1f1f15f0
Allen Obayagbona <iykeoba@gmail.com>
2020-09-30 11:50:59 -0400

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,267 @@
+
+"""
+CreateOrderFunction
+"""
+
+# import asyncio
+# import concurrent
+import datetime
+import json
+import os
+import logging
+from typing import List, Tuple
+from urllib.parse import urlparse
+import uuid
+import boto3
+# import jsonschema
+import requests
+## QLDB
+import amazon.ion.simpleion as simpleion
+from pyqldb.driver.qldb_driver import QldbDriver
+
+LEDGER = os.environ["LEDGER"]
+TABLE_NAME = os.environ["TABLE_NAME"]
+REGION = os.environ["REGION"]
+REGION2 = os.environ["REGION2"]
+
+# Instantiating the driver
+qldb_driver = QldbDriver(ledger_name=LEDGER)
+dynamodb = boto3.resource("dynamodb") # pylint: disable=invalid-name
+table = dynamodb.Table(TABLE_NAME) # pylint: disable=invalid-name,no-member
+
+productQty = []
+
+def validate_inventory(products: List[dict]) -> List[dict]:
+    """
+    Validate the inventory of the products in the order
+    """
+
+    for product in products:
+        response = table.get_item(
+            Key={
+                'productid': product['productId']
+            }
+        )
+        qty = response['Item']['QtyinStock']
+        qtyinregion = response['Item']['QtyinRegion']
+        name = response['Item']['Productname']
+        price = response['Item']["Price"]
+
+        if qty < product.get("quantity", 1):
+            return (False, "Not enough inventory for {}".format(product['name']))
+        else:
+            newqty = qty - product.get("quantity", 1)
+
+        if qtyinregion[REGION] < product.get("quantity", 1):
+            newqtyinregion1 = 0
+            newqtyinregion2 = qtyinregion[REGION2] - (product.get("quantity", 1) - qtyinregion[REGION])
+            #store data for rollback
+            oldprodqty1 = qtyinregion[REGION]
+            oldprodqty2 = product.get("quantity", 1) - qtyinregion[REGION]
+        else:
+            newqtyinregion1 = qtyinregion[REGION] - product.get("quantity", 1)
+            newqtyinregion2 = qtyinregion[REGION2]
+            #store data for rollback
+            oldprodqty1 = product.get("quantity", 1)
+            oldprodqty2 = 0
+
+        productQty.append({
+            "productId": product["productId"],
+            "name": name,
+            "price": price,
+            "prodquantity": product.get("quantity", 1),
+            "quantity": newqty,
+            "oldqtyinregion1": oldprodqty1,
+            "oldqtyinregion2": oldprodqty2,
+            "qtyinregion1": newqtyinregion1,
+            "qtyinregion2": newqtyinregion2
+        })
+        product["name"] = name
+        product["price"] = price
+        product["quantity"]: product.get("quantity", 1)
+
+    return (True, "There are enough product inventory to place the order")
+
+
+def validate_order(products: List[dict]) -> List[dict]:
+    """
+    Validate the inventory of the products in the order
+    """
+
+    for product in products:
+        response = table.get_item(
+            Key={
+                'productid': product['productId']
+            }
+        )
+        qtyinregion1 = response['Item']['QtyinRegion'][REGION]
+        qtyinregion2 = response['Item']['QtyinRegion'][REGION2]
+
+        if qtyinregion2 < 0 or qtyinregion1 < 0:
+            return (False, "Order cannot be accepted. Rolling back.")
+    return (True, "This order is good to go")
+
+
+def cleanup_products(products: List[dict]) -> List[dict]:
+    """
+    Cleanup products
+    """
+
+    return [{
+        "productId": product["productId"],
+        "quantity": product.get("quantity", 1)
+    } for product in products]
+
+
+def inject_order_fields(order: dict) -> dict:
+    """
+    Inject fields into the order and return the order
+    """
+
+    now = datetime.datetime.now()
+
+    order["orderId"] = str(uuid.uuid4())
+    order["status"] = "PENDING"
+    order["createdDate"] = now.isoformat()
+    order["modifiedDate"] = now.isoformat()
+    order["total"] = sum([p["price"]*p.get("quantity", 1) for p in order["products"]])
+
+    return order
+
+ 
+def store_order(transaction_executor, arg_1):
+    """
+    Store the order in QLDB
+    """
+    transaction_executor.execute_statement("INSERT INTO Orders ?", arg_1)
+
+def update_order(transaction_executor, order_id, status):
+    """
+    Update the order in QLDB
+    """
+    transaction_executor.execute_statement("UPDATE Orders SET status = ?  WHERE orderId = ?", status, order_id)
+
+def update_inventory(products: List[dict]) -> List[dict]:
+    """
+    Update the inventory of the products in the order
+    """
+    for product in products:
+        table.update_item(
+            Key={
+                'productid': product['productId']
+            },
+            UpdateExpression="SET QtyinStock = :val1, #qty.#reg = :val2, #qty.#reg2 = :val3",
+            ExpressionAttributeNames={
+                '#qty': 'QtyinRegion',
+                '#reg': REGION,
+                '#reg2': REGION2
+            },
+            ExpressionAttributeValues={
+                ':val1': product['quantity'],
+                ':val2': product['qtyinregion1'],
+                ':val3': product['qtyinregion2']
+            }
+        )
+    return (True, "Inventory updated")
+
+def rollback_inventory(products: List[dict]) -> List[dict]:
+    """
+    Rooback the order
+    """
+    for product in products:
+        table.update_item(
+            Key={
+                'productid': product['productId']
+            },
+            UpdateExpression="SET QtyinStock = QtyinStock + :val1, #qty.#reg = :qty.:reg + :val2, #qty.#reg2 = :qty.:reg2 + :val3",
+            ExpressionAttributeNames={
+                '#qty': 'QtyinRegion',
+                '#reg': REGION,
+                '#reg2': REGION2
+            },
+            ExpressionAttributeValues={
+                ':qty': 'QtyinRegion',
+                ':reg': REGION,
+                ':reg2': REGION2,
+                ':val1': product['prodquantity'],
+                ':val2': product['oldqtyinregion1'],
+                ':val3': product['oldqtyinregion2']
+            }
+        )
+    return (True, "Inventory updated")
+
+
+################### Start Handler  ################### 
+######################################################
+
+def lambda_handler(event, context):
+    """
+    Lambda function handler
+    """
+
+    # Basic checks on the event
+    for key in ["order", "userId"]:
+        if key not in event:
+            return {
+                "success": False,
+                "message": "Invalid event",
+                "errors": ["Missing {} in event".format(key)]
+            }
+
+    # Inject userId into the order
+    order = event["order"]
+    order["userId"] = event["userId"]
+
+    # Cleanup products
+    # order["products"] = cleanup_products(order["products"])
+    validate_inventory(order["products"])
+
+    # Inject fields in the order
+    order = inject_order_fields(order)
+
+    # create the Ion doc
+    ion_order = simpleion.loads(simpleion.dumps(order))
+
+    # Place order
+    try:
+        update_inventory(productQty)
+        qldb_driver.execute_lambda(lambda x: store_order(x, ion_order))
+    except:
+        return {
+            "success": False,
+            "message": "Issue placing order"
+        }
+
+    ready = "READY FOR FULFILLMENT"
+    cancel = "CANCELLED"
+
+    #confirm order
+    orderstatus = validate_order(order["products"])
+    if orderstatus:
+        qldb_driver.execute_lambda(lambda x: update_order(x, order["orderId"], ready))
+    else:
+        rollback_inventory(productQty)
+        qldb_driver.execute_lambda(lambda x: update_order(x, order["orderId"], cancel))
+        return {
+            "success": False,
+            "order": order,
+            "message": "Order not completed. Please try again."
+        }
+
+    ## Log
+    # tracer.put_annotation("orderId", order["orderId"])
+    # logger.info({
+    #     "message": "Order {} created".format(order["orderId"]),
+    #     "orderId": order["orderId"]
+    # })
+    # logger.debug({
+    #     "message": "Order {} created".format(order["orderId"]),
+    #     "orderId": order["orderId"],
+    #     "order": order
+    # })
+
+    return {
+        "success": True,
+        "order": order,
+        "message": "Order created"
+    }
